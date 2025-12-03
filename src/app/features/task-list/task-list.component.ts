b6a88391c15f9fb81499b80a82fbc565450c71c2
VukasinVujic <vukasinvujic021@gmail.com>
2025-12-03 18:52:13 +0100

    feat(tasks): add kanban-style board with status-based columns

--- src/app/features/task-list/task-list.component.ts
+++ src/app/features/task-list/task-list.component.ts
@@ -1,7 +1,8 @@
 import { Component, inject } from '@angular/core';
 import { TaskStoreService } from '../../core/services/task-store.service';
 import { CommonModule } from '@angular/common';
-import { TaskStatus } from '../../core/models/task.model';
+import { TaskStatus, Task } from '../../core/models/task.model';
+import { map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-task-list',
@@ -12,9 +13,19 @@ import { TaskStatus } from '../../core/models/task.model';
 })
 export class TaskListComponent {
   protected readonly taskStore = inject(TaskStoreService);
-  statuses: TaskStatus[] = ['todo', 'done', 'in-progress'];
+  statuses: TaskStatus[] = ['todo', 'in-progress', 'done'];
 
   public tasks$ = this.taskStore.tasks$;
+
+  todo$ = this.getTasksByStatus('todo');
+  inProgress$ = this.getTasksByStatus('in-progress');
+  done$ = this.getTasksByStatus('done');
+
+  private getTasksByStatus(status: TaskStatus): Observable<Task[]> {
+    return this.tasks$.pipe(
+      map((tasks) => tasks.filter((t) => t.status === status))
+    );
+  }
 
   onChangeStatus(id: string, status: TaskStatus) {
     this.taskStore.updateTask(id, status);
